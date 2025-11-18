6c2c31fa4159fc6936bab4dcabdbdf7ebdc831ca
Edson Leonardo <edsonleonardosanchezmontalvo@gmail.com>
2025-11-17 21:02:14 -0600

    alerts

--- Front/scripts/app.js
+++ Front/scripts/app.js
@@ -9,11 +9,10 @@ const formLogin = document.getElementById("formLogin");
 const userName = document.getElementById("userName");
 const userIcon = document.getElementById("userIcon");
 
-/*Mensaje para edson */
-/*En este app.js tienes que añadir toda la lógica de tu captcha, 
-mas aparte haste las verificaciones básicas para que funcione el regístro
-y lógin, obvio como te dije, nada con apis, sólo create unos condicionales
-así tipo if(regUser===edson){alert(simon carnal)}else{alert(pendejo estás mal)} */
+
+
+
+
 
 logOutBtn.style.display = "none";
 userIcon.style.display = "none";
@@ -35,29 +34,35 @@ RegBtn.addEventListener("click", () => {
   regModal.style.display = "block";
 });
 
+//Login
 formLogin.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const user = document.getElementById("login").value;
-  const pass = document.getElementById("password").value;
+  const user = document.getElementById("login").value.trim();
+  const password = document.getElementById("password").value.trim();
 
-  if (user === "admin" && pass === "123") {
+
+
+  if (user === "admin" && password === "123") {
+    toast("Sesión iniciada", "#00c851");
+
     localStorage.setItem("usuario", user);
     loginModal.style.display = "none";
 
     userName.textContent = user;
-
     userName.style.display = "inline";
     userIcon.style.display = "inline";
     logOutBtn.style.display = "inline";
     logInBtn.style.display = "none";
   } else {
-    alert("Usuario o contraseña incorrectos");
+    toast("Usuario o contraseña incorrectos", "#ff4444");
   }
 });
 
+
 logOutBtn.addEventListener("click", () => {
   localStorage.removeItem("usuario");
+    toast("Adiosss ", "#ff4444");
 
   userName.style.display = "none";
   userIcon.style.display = "none";
@@ -75,4 +80,47 @@ window.addEventListener("load", () => {
     logOutBtn.style.display = "inline";
     logInBtn.style.display = "none";
   }
+});
+
+//  registro 
+const fakeUsers = ["leo", "juan", "admin"];
+
+formRegister.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = document.getElementById("regUser").value.trim();
+  const email = document.getElementById("regEmail").value.trim();
+  const pass1 = document.getElementById("regPass1").value.trim();
+  const pass2 = document.getElementById("regPass2").value.trim();
+
+  if (!user || !email || !pass1 || !pass2) {
+    toast("Llena todos los campos", "#ff4444");
+    return;
+  }
+
+  if (fakeUsers.includes(user.toLowerCase())) {
+    toast("Ese usuario ya existe", "#ff4444");
+    return;
+  }
+
+  if (!email.includes("@") || !email.includes(".")) {
+    toast("Correo invalido", "#ff4444");
+    return;
+  }
+
+
+  if (pass1 !== pass2) {
+    toast("Las contraseñas no coinciden", "#ff4444");
+    return;
+  }
+
+
+  toast("Registro exitoso", "#00c851");
+
+  regModal.style.display = "none";
+
+  document.getElementById("regUser").value = "";
+  document.getElementById("regEmail").value = "";
+  document.getElementById("regPass1").value = "";
+  document.getElementById("regPass2").value = "";
 });
