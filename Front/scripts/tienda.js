690caba680cad13901e2cb11adc6e3ead06f036b
JoseNarvaezMtz <jose.narvaez.mtz05@gmail.com>
2025-11-29 23:21:44 -0600

    Modificacion de display de carnalito de carga

--- Front/scripts/tienda.js
+++ Front/scripts/tienda.js
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function aplicarFiltros() {
     productsContainer.innerHTML = 
-      '<div style="position:absolute; left:44vw; top: 18vh; display:flex; flex-direction:column; gap:1vw; justify-content:center; align-items:center;><p style="text-align:center; width:100%; margin-top: 20px;">Cargando...</p> <img src="./media/carnalito.gif" alt=""></div>';
+      '<div style="position:absolute; left:46%; top: 18vh; display:flex; flex-direction:column; gap:1vw; justify-content:center; align-items:center;><p style="text-align:center; width:100%; margin-top: 20px;">Cargando...</p> <img src="./media/carnalito.gif" alt=""></div>';
 
     actualizarInterfaz();
 
