470f211814fbee15770b0c91b90db2dce84f65f2
Harim Dueñas <harimenriqueduenas@outlook.com>
2025-12-01 10:20:25 -0600

    Correccion de detallitos al enviar los datos del front al back, cambio de alerts

--- Front/scripts/contacto.js
+++ Front/scripts/contacto.js
@@ -3,24 +3,32 @@ import servicios from "./servicios.js";
 const formContacto = document.getElementById("formularioContacto");
 
 if (formContacto) {
-    formContacto.addEventListener("submit", async (e) => {
-        e.preventDefault();
+  formContacto.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-        const datos = {
-            nombre: document.getElementById("nombre").value,
-            email: document.getElementById("email").value, 
-            mensaje: document.getElementById("comentario").value
-        };
+    const datos = {
+      nombre: document.getElementById("nombre").value,
+      email: document.getElementById("email").value,
+      mensaje: document.getElementById("comentario").value,
+    };
 
-        try {
-            const data = await servicios.enviarCorreoContacto(datos);
+    try {
+      const data = await servicios.enviarCorreoContacto(datos);
 
-            if (data.status === "success") {
-                alert("Mensaje enviado correctamente. Te contactaremos pronto.");
-                formContacto.reset();
-            }
-        } catch (error) {
-            alert(error.message);
-        }
-    });
+      if (data.status === "success") {
+        Swal.fire({
+          title: `Mensaje enviado correctamente. Te contactaremos pronto.`,
+          icon: "success",
+          confirmButtonText: "Ok",
+        });
+        formContacto.reset();
+      }
+    } catch (error) {
+      Swal.fire({
+        title: `Error: ${error.message}`,
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
+    }
+  });
 }
--- Front/scripts/suscripcion.js
+++ Front/scripts/suscripcion.js
@@ -2,25 +2,33 @@ import servicios from "./servicios.js";
 
 // AQUI PONGAN LOS ID´S QUE MANEJEN
 const btnSuscripcion = document.getElementById("btn-suscripcion"); // Boton de suscribirse
-const inputEmail = document.getElementById("input-email-suscripcion") // Input del correo 
+const inputEmail = document.getElementById("input-email-suscripcion"); // Input del correo
 
 if (btnSuscripcion) {
-    btnSuscripcion.addEventListener("click", async (e) => {
-        e.preventDefault();
+  btnSuscripcion.addEventListener("click", async (e) => {
+    e.preventDefault();
 
-        if (!inputEmail.value) return alert("Escribe tu correo");
+    if (!inputEmail.value) return alert("Escribe tu correo");
 
-        try {
-            const data = await servicios.enviarCorreoSuscripcion(inputEmail.value);
-                        
-            if(data.status === "success") {
-                alert("¡Suscripción exitosa! Revisa tu correo para ver tu cupon.");
-                inputEmail.value = ""; // Limpiar input
-            } else {
-                alert("Error: " + data.message);
-            }
-        } catch (error) {
-            alert(error.message);
-        }
-    });
+    try {
+      const data = await servicios.enviarCorreoSuscripcion(inputEmail.value);
+
+      if (data.status === "success") {
+        Swal.fire({
+          title: "¡Suscripción exitosa! Revisa tu correo para ver tu cupon.",
+          icon: "success",
+          confirmButtonText: "Ok",
+        });
+        inputEmail.value = ""; // Limpiar input
+      } else {
+        Swal.fire({
+            title: `Error: ${data.message}`,
+            icon: "error",
+            confirmButtonText: "Ok",
+        });
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  });
 }
