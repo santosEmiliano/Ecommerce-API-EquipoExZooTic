4d17846f8a2f45e920968e0488795703be99db4c
JoseNarvaezMtz <jose.narvaez.mtz05@gmail.com>
2025-12-05 13:20:13 -0600

    Ultimos detalles de estructura del backend y correccion de un alert

--- Back/routes/chat.routes.js
+++ Back/routes/chat.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const chatController = require('../controllers/chatController');
+const chatController = require('../controllers/chat.controller');
 
 router.post('/', chatController.chatWithGemini);
 
