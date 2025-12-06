b734598033c15d49750f12a0fc89da127a8c1c52
Edson Leonardo <edsonleonardosanchezmontalvo@gmail.com>
2025-12-06 02:24:14 -0600

    prueba 2 app y validarCaptcha

--- Back/middleware/validarCaptcha.js
+++ Back/middleware/validarCaptcha.js
@@ -3,16 +3,21 @@ const { captchas } = require("../controllers/captcha.controller");
 module.exports = (req, res, next) => {
   const { captchaId, captcha } = req.body;
 
+  if (!captchaId || !captcha) {
+    return res.status(400).json({ success: false, message: "Captcha y captchaId son requeridos." });
+  }
+
   const guardado = captchas[captchaId];
 
+  console.log("req.body:", req.body);
+  console.log("captchaId:", captchaId, "captcha:", captcha);
+  console.log("guardado:", guardado);
+
   if (!guardado || guardado.toLowerCase() !== captcha.toLowerCase()) {
-    console.log("req.body:", req.body);
-    console.log("captchaId:", captchaId, "captcha enviado:", captcha);
-    console.log("captcha guardado:", guardado);
     return res.status(400).json({ success: false, message: "Captcha inválido." });
   }
 
-  // Si es correcto, eliminarlo para que no se use otra vez
+  // Ya usado, eliminarlo
   delete captchas[captchaId];
 
   next();
