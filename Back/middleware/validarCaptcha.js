fcdf900363aafca4ba59448d3e6f51aa9a5fae6d
Edson Leonardo <edsonleonardosanchezmontalvo@gmail.com>
2025-12-06 02:38:42 -0600

    Prueba 4 validarCaptcha

--- Back/middleware/validarCaptcha.js
+++ Back/middleware/validarCaptcha.js
@@ -7,14 +7,18 @@ module.exports = (req, res, next) => {
   console.log("CaptchaId recibido:", captchaId);
 
   const guardado = captchas[captchaId];
+
+  // validar existencia antes de usar toLowerCase()
   if (!guardado) {
     return res.status(400).json({ success: false, message: "Captcha inválido o expirado." });
   }
 
-  if (guardado.toLowerCase() !== captcha.toLowerCase()) {
+  if (guardado !== captcha.toLowerCase()) {
     return res.status(400).json({ success: false, message: "Captcha incorrecto." });
   }
 
+  // si es correcto, eliminarlo para que no se use otra vez
   delete captchas[captchaId];
+
   next();
 };
