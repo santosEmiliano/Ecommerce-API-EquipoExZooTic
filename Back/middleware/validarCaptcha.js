9f1bf843708d26220df876f6ac842e5840fe5c6a
Diana Ruth <llol7452@gmail.com>
2025-12-06 01:45:44 -0600

    cambio5

--- Back/middleware/validarCaptcha.js
+++ Back/middleware/validarCaptcha.js
@@ -1,14 +1,15 @@
 const { captchas } = require("../controllers/captcha.controller");
 
 module.exports = (req, res, next) => {
-  const { captchaId, captcha: captchaTxt } = req.body;
+  const { captchaId, captcha } = req.body;
 
   const guardado = captchas[captchaId];
 
   console.log("req.body:", req.body);
-  console.log("captchaId:", captchaId, "captchaTxt:", captchaTxt);
+  console.log("captchaId:", captchaId, "captchaTxt:", captcha);
 
-  if (!guardado || guardado.toLowerCase() !== captchaTxt.toLowerCase()) {
+
+  if (!guardado || guardado.toLowerCase() !== captcha.toLowerCase()) {
   return res.status(400).json({ success: false, message: "Captcha inválido." });
 }
 
